3f2496523c8798cb04c3bab7404c7cddf9486ddf
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2026-07-20 13:32:29 -0300

    feat: enable global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import cookieParser from 'cookie-parser'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.use(cookieParser())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
 
   app.enableCors({
     origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
